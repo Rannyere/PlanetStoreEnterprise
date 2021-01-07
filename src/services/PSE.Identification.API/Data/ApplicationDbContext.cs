aa5e0e31e6e8ead5218b25340245e2100c67e7f4
Rannyere Almeida <rannyalmeida27@hotmail.com>
2021-01-07 11:26:03 +0100

    configure Identity and database generate with EntityFramework

---
+++ src/services/PSE.Identification.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace PSE.Identification.API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+    }
+}
