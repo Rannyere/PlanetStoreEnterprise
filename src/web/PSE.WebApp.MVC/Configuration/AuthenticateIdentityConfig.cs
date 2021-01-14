ab2edb9a75863f018f4e684ea7717b90180502c2
Rannyere Almeida <rannyalmeida27@hotmail.com>
2021-01-14 21:03:14 +0100

    configure authentication with cookie  and adjust startup

---
+++ src/web/PSE.WebApp.MVC/Configuration/AuthenticateIdentityConfig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PSE.WebApp.MVC.Configuration
+{
+    public static class AuthenticateIdentityConfig
+    {
+        public static void AddAuthenticateIdentityConfiguration(this IServiceCollection services)
+        {
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                .AddCookie(options =>
+                {
+                    options.LoginPath = "/login";
+                    options.AccessDeniedPath = "/denied-access";
+                });    
+        }
+
+        public static void UseAuthenticateIdentityConfiguration(this IApplicationBuilder app)
+        {
+            app.UseAuthentication();
+            app.UseAuthorization();
+        }
+    }
+}
---
+++ src/web/PSE.WebApp.MVC/Controllers/IdentificationController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PSE.WebApp.MVC.Controllers
+{
+    public class IdentificationController : Controller
+    {
+        
+    }
+}
