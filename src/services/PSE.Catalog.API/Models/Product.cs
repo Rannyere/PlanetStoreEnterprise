8621c04f18cbb3ac8c53f6c6b5977c5e600d2d0f
Rannyere Almeida <rannyalmeida27@hotmail.com>
2021-01-20 08:02:43 +0100

    building base abstract class Entity and defining model products to Catalog.API

---
+++ src/services/PSE.Catalog.API/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using PSE.Core.DomainObjects;
+
+namespace PSE.Catalog.API.Models
+{
+    public class Product : Entity
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Activ { get; set; }
+        public decimal Value { get; set; }
+        public DateTime DateRegister { get; set; }
+        public string Image { get; set; }
+        public int QuantityStock { get; set; }
+    }
+}
