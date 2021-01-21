0ab2c809345945e5acddf745bb25db09e8a3d4b0
Rannyere Almeida <rannyalmeida27@hotmail.com>
2021-01-21 11:43:16 +0100

    Validation of user access using Claims

--- src/services/PSE.Catalog.API/Configuration/SwaggerConfig.cs
+++ src/services/PSE.Catalog.API/Configuration/SwaggerConfig.cs
@@ -18,6 +18,31 @@ namespace PSE.Catalog.API.Configuration
                     Contact = new OpenApiContact() { Name = "Rannyere Almeida", Email = "rannyalmeida27@hotmail.com" },
                     //License = new OpenApiLicense() { Name = "MIT", Url = new Uri("https://opensource.org/licenses/MIT") }
                 });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Insert the JWT token like this: Bearer {your token}",
+                    Name = "Authorization",
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                    }
+                });
             });
         }
 
--- src/services/PSE.Catalog.API/Controllers/CatalogController.cs
+++ src/services/PSE.Catalog.API/Controllers/CatalogController.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using PSE.Catalog.API.Models;
+using PSE.WebAPI.Core.Identification;
 
 namespace PSE.Catalog.API.Controllers
 {
     [ApiController]
+    [Authorize]
     public class CatalogController : Controller
     {
         private readonly IProductRepository _productRepository;
@@ -16,12 +19,14 @@ namespace PSE.Catalog.API.Controllers
             _productRepository = productRepository;
         }
 
+        [AllowAnonymous]
         [HttpGet("catalog/products")]
         public async Task<IEnumerable<Product>> Index()
         {
             return await _productRepository.GetAll();
         }
 
+        [ClaimsAuthorize("Catalog", "Read")]
         [HttpGet("catalog/products/{id}")]
         public async Task<Product> ProductDetail(Guid id)
         {
