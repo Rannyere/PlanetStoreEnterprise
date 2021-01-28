480d76da0a2f60f9d3d85b9af29ba9e8f3b0f17d
Rannyere Almeida <rannyalmeida27@hotmail.com>
2021-01-28 11:46:45 +0100

    validation RegisterCustomerCommand with fluentValidation - PSE.Clientes.API

--- src/services/PSE.Clients.API/Application/Commands/RegisterCustomerCommand.cs
+++ src/services/PSE.Clients.API/Application/Commands/RegisterCustomerCommand.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using FluentValidation;
 using PSE.Core.Messages;
 
 namespace PSE.Clients.API.Application.Commands
@@ -24,7 +25,40 @@ namespace PSE.Clients.API.Application.Commands
 
         public override bool IsValid()
         {
-            return base.IsValid();
+            ValidationResult = new RegisterCustomerValidation().Validate(this);
+            return ValidationResult.IsValid;
+        }
+    }
+
+    public class RegisterCustomerValidation : AbstractValidator<RegisterCustomerCommand>
+    {
+        public RegisterCustomerValidation()
+        {
+            RuleFor(c => c.Id)
+                .NotEqual(Guid.Empty)
+                .WithMessage("Id Customer invalid");
+
+            RuleFor(c => c.Name)
+                    .NotEmpty()
+                    .WithMessage("The Customer name was not informed");
+
+            RuleFor(c => c.Cpf)
+                    .Must(HasCpfValid)
+                    .WithMessage("The CPF informed is not valid.");
+
+            RuleFor(c => c.Email)
+                .Must(HasEmailValid)
+                .WithMessage("The E-mail informed is not valid.");
+        }
+
+        protected static bool HasCpfValid(string cpf)
+        {
+            return Core.DomainObjects.Cpf.ValidateCpfBrazil(cpf);
+        }
+
+        protected static bool HasEmailValid(string email)
+        {
+            return Core.DomainObjects.Email.ValidateEmail(email);
         }
     }
 }
