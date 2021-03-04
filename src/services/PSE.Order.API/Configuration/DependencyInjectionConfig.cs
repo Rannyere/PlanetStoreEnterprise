0fc11b65b7e548f16e541fcad8d02234dfe78b51
Rannyere Almeida <rannyalmeida27@hotmail.com>
2021-03-04 11:46:02 +0100

    create VoucherController PSE.Order.API

--- src/services/PSE.Order.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/PSE.Order.API/Configuration/DependencyInjectionConfig.cs
@@ -1,6 +1,11 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using PSE.Core.Mediator;
+using PSE.Order.API.Application.Queries;
+using PSE.Order.Domain.Vouchers;
 using PSE.Order.Infra.Data;
+using PSE.Order.Infra.Data.Repository;
+using PSE.WebAPI.Core.User;
 
 namespace PSE.Order.API.Configuration
 {
@@ -8,9 +13,17 @@ namespace PSE.Order.API.Configuration
     {
         public static void RegisterServices(this IServiceCollection services)
         {
-            services.AddScoped<OrderDbContext>();
+            // API
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddScoped<IAspNetUser, AspNetUser>();
 
+            // Application
             services.AddScoped<IMediatorHandler, MediatorHandler>();
+            services.AddScoped<IVoucherQueries, VoucherQueries>();
+
+            // Data
+            services.AddScoped<IVoucherRepository, VoucherRepository>();
+            services.AddScoped<OrderDbContext>();
         }
     }
 }
---
+++ src/services/PSE.Order.API/Controllers/VoucherController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using PSE.Order.API.Application.DTOs;
+using PSE.Order.API.Application.Queries;
+using PSE.WebAPI.Core.Controllers;
+
+namespace PSE.Order.API.Controllers
+{
+    [Authorize]
+    public class VoucherController : MainController
+    {
+        private readonly IVoucherQueries _voucherQueries;
+
+        public VoucherController(IVoucherQueries voucherQueries)
+        {
+            _voucherQueries = voucherQueries;
+        }
+
+        [HttpGet("voucher/{code}")]
+        [ProducesResponseType(typeof(VoucherDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<IActionResult> ObterPorCodigo(string code)
+        {
+            if (string.IsNullOrEmpty(code)) return NotFound();
+
+            var voucher = await _voucherQueries.GetVoucherByCode(code);
+
+            return voucher == null ? NotFound() : CustomResponse(voucher);
+        }
+    }
+}
