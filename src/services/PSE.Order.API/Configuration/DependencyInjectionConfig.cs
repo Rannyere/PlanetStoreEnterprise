3aa8acc760ac5b9b0ee66ab0e366dcb68ddb2de1
Rannyere Almeida <rannyalmeida27@hotmail.com>
2021-03-13 09:49:47 +0100

    build controller PSE.Order.API

--- src/services/PSE.Order.API/Configuration/DependencyInjectionConfig.cs
+++ src/services/PSE.Order.API/Configuration/DependencyInjectionConfig.cs
@@ -1,7 +1,13 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FluentValidation.Results;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using PSE.Cart.API.Application.Events;
 using PSE.Core.Mediator;
+using PSE.Order.API.Application.Commands;
+using PSE.Order.API.Application.Events;
 using PSE.Order.API.Application.Queries;
+using PSE.Order.Domain.Orders;
 using PSE.Order.Domain.Vouchers;
 using PSE.Order.Infra.Data;
 using PSE.Order.Infra.Data.Repository;
@@ -17,11 +23,19 @@ namespace PSE.Order.API.Configuration
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddScoped<IAspNetUser, AspNetUser>();
 
+            // Commands
+            services.AddScoped<IRequestHandler<ExecuteOrderCommand, ValidationResult>, OrderCommandHandler>();
+
+            // Events
+            services.AddScoped<INotificationHandler<OrderExecutedEvent>, OrderEventHandler>();
+
             // Application
             services.AddScoped<IMediatorHandler, MediatorHandler>();
             services.AddScoped<IVoucherQueries, VoucherQueries>();
+            services.AddScoped<IOrderQueries, OrderQueries>();
 
             // Data
+            services.AddScoped<IOrderRepository, OrderRepository>();
             services.AddScoped<IVoucherRepository, VoucherRepository>();
             services.AddScoped<OrderDbContext>();
         }
